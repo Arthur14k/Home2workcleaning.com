bbd12e132117d1a44e4c0a7b7f94a525ea90bf25
Arthur14k <14k795@gmail.com>
2025-07-31 00:04:30 +0100

    Add files via upload

---
+++ supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = https://ariabxxuqnvidyilclcx.supabase.co
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmhfZ...
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
