3d19c32dbf3fa59b6d9127690d19a192bb49e9fc
Arthur14k <14k795@gmail.com>
2025-08-21 22:28:01 +0100

    Add files via upload

---
+++ app/contact/head.tsx
@@ -0,0 +1,5 @@
+export const metadata = {
+  title: "Contact Us – Home2Work Cleaning | Get Your Free Quote Today",
+  description:
+    "Reach out to Home2Work Cleaning for a free consultation and tailored cleaning solutions.",
+};
