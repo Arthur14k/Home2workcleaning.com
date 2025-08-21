a1234004e5ba7ac2cf7a65dd43337d6f9ee7a936
Arthur14k <14k795@gmail.com>
2025-08-21 21:49:49 +0100

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,5 @@
 "use client"
 
-export const metadata = {
-  title: "Contact Us – Home2Work Cleaning | Get Your Free Quote Today",
-  description: "Reach out to Home2Work Cleaning for a free consultation and tailored cleaning solutions.",
-};
-
 import type React from "react"
 
 import { useState } from "react"
